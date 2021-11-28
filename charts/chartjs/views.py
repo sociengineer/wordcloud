08d96ce10729f7975f4c010ba47da64d319e47ea
Wookjae Maeng <wookjae@eigene.ai>
2021-11-28 23:14:01 +0900

    first commit

---
+++ charts/chartjs/views.py
@@ -0,0 +1,27 @@
+# from django.http import JsonResponse
+   
+from django.shortcuts import render
+from django.views.generic import View
+   
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.core.files.storage import default_storage
+import pandas as pd
+   
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chartjs/index.html')
+   
+
+
+data = []
+df = pd.read_csv('chartjs/data/worddata.csv')
+for index, review in df.iterrows():
+    temp_dict = {}
+    temp_dict['text'] = review['text']
+    temp_dict['size'] = review['frequency']
+    data.append(temp_dict)
+   
+class ChartData(APIView):   
+    def get(self, request, format = None):
+        return Response(data)
